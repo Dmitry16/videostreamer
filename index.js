112378f71c20a75fe518e0e34078b49769cd3e1e
Dmitry16 <europartner@inbox.ru>
2018-11-08 21:16:54 +0200

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+const { createServer } = require('http')
+const { stat, createReadStream } = require('fs')
+const { promisify } = require('util')
+const fileInfo = promisify(stat)
+const file1 = './IMG_0687.m4v'
+
+createServer(async (req, res) => {
+    
+    const { size } = await fileInfo(file1)
+
+    res.writeHead(200, {
+        'Content-Length': size,
+        'Content-Type': 'video/mp4'
+    })
+    createReadStream(file1).pipe(res)
+
+}).listen(3001, () => { console.log('Server running on port 3001')})

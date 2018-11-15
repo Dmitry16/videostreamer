db3d5dbd3d69582de7747f97f460785604f26962
Dmitry16 <europartner@inbox.ru>
2018-11-15 10:16:04 +0200

    upload

--- index.js
+++ index.js
@@ -1,31 +1,63 @@
 const { createServer } = require('http')
-const { stat, createReadStream } = require('fs')
+const { stat, createReadStream, createWriteStream } = require('fs')
 const { promisify } = require('util')
+const multiparty = require('multiparty')
 const fileInfo = promisify(stat)
-const file = './video/IMG_0687.m4v'
+let file
 
-createServer(async (req, res) => {
-    
-    const { size } = await fileInfo(file)
-    const range = req.headers.range
-    if (range) {
-        let [start, end] = range.replace(/bytes=/, '').split('-')
-        start = parseInt(start, 10)
-        end = end ? parseInt(end, 10) : size - 1
-        // console.log('range', start, end)
-        res.writeHead(206, {
-            'Content-Range': `bytes ${start}-${end}/${size}`,
-            'Accept-Ranges': 'bytes',
-            'Content-Length': (end - start) + 1,
-            'Content-Type': 'video/mp4'
+const respondWithVideo = async (req, res, file) => {
+
+    if (file) { 
+        const { size } = await fileInfo(`./uploads/${file}`) 
+        const range = req.headers.range
+        if (range) {
+            let [start, end] = range.replace(/bytes=/, '').split('-')
+            start = parseInt(start, 10)
+            end = end ? parseInt(end, 10) : size - 1
+            res.writeHead(206, {
+                'Content-Range': `bytes ${start}-${end}/${size}`,
+                'Accept-Ranges': 'bytes',
+                'Content-Length': (end - start) + 1,
+                'Content-Type': 'video/mp4'
+            })
+            createReadStream(`./video/${file}`, { start, end }).pipe(res)
+        } else {
+            res.writeHead(200, {
+                'Content-Length': size,
+                'Content-Type': 'video/mp4'
+            })
+            createReadStream(`./uploads/${file}`).pipe(res)
+        }
+    }
+}
+
+createServer((req, res) => {
+    if (req.method === 'POST') {
+        const form = new multiparty.Form()
+        form.parse(req)
+        form.on('part', (part) => {
+            part.pipe(createWriteStream(`./uploads/${part.filename}`))
+            .on('close', () => {
+                file = part.filename
+                res.writeHead(200, { 'Content-Type': 'text/html' })
+                res.end(`
+                    <h1>File uploaded ${part.filename}</h1>
+                    <button>Play</button>
+                `)
+            })
         })
-        createReadStream(file, { start, end }).pipe(res)
-    } else {
-        res.writeHead(200, {
-            'Content-Length': size,
-            'Content-Type': 'video/mp4'
-        })
-        createReadStream(file).pipe(res)
+    } 
+    else if (req.url === '/video') {
+        respondWithVideo(req, res, file)
+    } 
+    else {
+        res.writeHead(200, { 'Content-Type': 'text/html'})
+        res.end(`
+            <form enctype="multipart/form-data" method="POST" action="/">
+                <input type="file" name="upload-file" />
+                <button>Upload File</button>
+            </form>
+        `)
     }
 
 }).listen(3001, () => { console.log('Server running on port 3001')})
